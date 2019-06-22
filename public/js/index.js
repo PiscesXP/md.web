4ba5a2716cd6725401de0ac51e7032a5b7f10e67
Zhenxi <PiscesXP@outlook.com>
2019-06-22 21:20:15 +0800

    最多每100ms刷新一次，解决卡顿问题.

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,18 @@
 const editor = document.querySelector("#markDownEditor");
 const outputNode = document.querySelector("#markDownOutput");
+const refreshInterval = 100;
+let timer = null;
 if (editor) {
     //初始化时生成一次
     mountMarkdown(editor.value, outputNode);
     //添加listener
     editor.addEventListener("input", () => {
-            mountMarkdown(editor.value, outputNode);
+            if (timer === null) {
+                timer = setTimeout(() => {
+                    mountMarkdown(editor.value, outputNode);
+                    timer = null;
+                }, refreshInterval);
+            }
         }
     )
 } else {
